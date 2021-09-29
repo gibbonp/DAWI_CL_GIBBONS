995a6ff4c5182120524e938d062b7cf338326842
gibbonp <pablo.emanuel.gibbons@gmail.com>
2021-09-29 11:24:44 -0500

    update EC1-GIBBONS

--- src/main/java/app/RegistrarPelicula.java
+++ src/main/java/app/RegistrarPelicula.java
@@ -4,10 +4,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import model.Pelicula;
+
 public class RegistrarPelicula {
 	public static void name() {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql8");
 		EntityManager em = fabrica.createEntityManager();
+		
+		Pelicula p = new Pelicula();
 		
 		
 		
